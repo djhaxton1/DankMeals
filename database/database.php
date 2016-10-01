858bec25dfb48d5248280193de6a3440e2575fe0
Dustin J Haxton <djhaxton@mtu.edu>
2016-10-01 17:43:13 -0400

    Converted the database.php format into a class for modularity purposes

--- database/database.php
+++ database/database.php
@@ -1,23 +1,54 @@
 <?php
-// Database File
-// The PHP interface with our server
-$server   = "141.219.196.115:3306";
-$username = "tsp";
-$password = "null";
+/**
+ * Class: Database
+ * Purpose : An Interface with the MySQL Database used by 
+ */
+class database {
+	/* Private Data Members */
+	private $conn;
+	
+	/**
+	 * Instaintates an new connection to the database
+	 */
+	function __construct() {
+		$server   = "141.219.196.115";
+		$username =             "tsp";
+		$password =            "null";
+		$db       =           "tspdb";
+		// Create a Connection
+		$this->conn  = new mysqli($server, $username, $password, $db);
 
-// open an SSH session with the remote server
-$db       = "tspdb";
-
-// Create a Connection
-$conn  = mysqli_connect("$server", "$username", "$password");
-
-// Verify a Connection was made
-if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+		// Verify a Connection was made
+		if ($this->conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} else {
+			echo ("Connected Successfully\n");
+		}
+		$cmd = "show databases";
+		$result = mysqli_query($this->conn, $cmd);
+		if ($result->num_rows > 0) {
+			// output data of each row
+			$i = 1;
+			while($row = $result->fetch_assoc()) {
+				echo "Database $i: " . $row["Database"] . "\n" ;
+				$i = $i + 1;
+			}
+		} else {
+			echo "0 results";
+		}
+	}
+	
+	
+	/**
+	 * Closes the Connection to the database.
+	 */
+	function __destruct() {
+		echo "Closing Connection to Database...";
+		$this->conn->close();
+		echo "Done\n";
+	}
 }
 
-// Ask for all databases
-$cmd = "show databases";
-
-mysqli_query($conn, $cmd);
+$db = new database();
+$db = null;
 ?>

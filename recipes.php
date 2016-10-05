5a83b16efcdccb0c12e676dea747dfdbef58bae3
Michael Miller <mikemandm222@gmail.com>
2016-10-05 12:29:13 -0400

    Basic recipe data now appears in index.html
    added an ajax call to recipe.php which returns a table in html

--- recipes.php
+++ recipes.php
@@ -1,15 +1,13 @@
 <?php
   include './database/database.php';
-
   $db = new database();
   $result = $db->sendCommandParse("SELECT * FROM recipes;", array("id", "title"));
-  $returnString = '<table>';
-  
-  for($j = 0; $j < count($result); $j++) {
-    $returnString .= "<tr><td>".$result[$j]."</td><td>".$result[$j+1]."</td></tr>";
-  }
-  
-  $returnString .= "</table>";
+  $returnString = "<table class='table'><thead><tr><th>Recipe ID</th><th>Title</th></tr></thead><tbody>";
+  for($j = 1; $j < count($result) - 1; $j += 2) {
+   $returnString .= "<tr><td>".$result[$j]."</td><td>".$result[$j+1]."</td></tr>";
+ }
+
+  $returnString .= "</tbody></table>";
   echo $returnString;
   $db = null;
 ?>

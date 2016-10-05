1c749ae3895b092b7bce1160b62e706b1a25f4c1
mslay <Michaellay1313@gmail.com>
2016-10-05 15:55:19 -0400

    Revert "Added Support for Filling in Recipe Info"
    This reverts commit 2c522de3ed5336d1633a978e80625f4c0e9c33d1.

--- getRecipe.php
+++ getRecipe.php
@@ -7,89 +7,88 @@
  */
 require 'database\database.php';
 
-if ($_POST["function"] == "getTitle()"){
-    echo getTitle($_POST["argument"]);
-}elseif ($_POST["function"] == "getIngredients()"){
-    echo getIngredients($_POST["argument"]);
-}elseif ($_POST["function"] == "getInstructions()"){
-    echo getInstructions($_POST["argument"]);
-}else{
-    echo "Error: Function does not exist!";
-}
-
-
 /**
- * @param $id           integer id of recipe
- * @return string       string that contains all relevant information as html tags
- * information retrieved   |  title
+ * @param $id        the integer id of the recipe requested
+ * @return array     an array with three string elements where:
+ *                   [1]   information from recipes table
+ *                   [2]   information from ingredients table
+ *                   [3]   information from instructions table
  *
- * Returns the title of a recipe entry given its id
+ * Gathers all necessary data for a recipe from the database provided its id
+ * and return it as an array of strings formatted as html tags
  */
-function getTitle($id){
-    ob_start();
-    $db = new database();
-    ob_end_clean();
-    $query = "SELECT * FROM recipes WHERE id=" . $id;
-    $relevant = array("title");
-    $result = $db->sendCommandParse($query, $relevant);
-    ob_start();
-    $db = null;
-    ob_end_clean();
-    $output = $result[1];
+function getRecipe($id){
+    $output = array();
+    $output[1] = getMain($id);
+    $output[2] = getIngredients($id);
+    $output[3] = getInstructions($id);
     return $output;
 }
 
 /**
  * @param $id           integer id of recipe
  * @return string       string that contains all relevant information as html tags
- * information retrieved   |  <li> instruction_text </li>
+ * information retrieved   |  <h1> title </h1>
+ *
+ * Gathers all of the relevant information stored in the recipes table and
+ * returns it as a string formatted as html tags
+ */
+function getMain($id){
+    $db = new database();
+    $query = "SELECT * FROM recipes WHERE id=" . $id;
+    $relevant = array("title");
+    $result = $db->sendCommandParse($query, $relevant);
+    $db = null;
+    $output = "<h1>" . $result[1] . "</h1>";
+    return $output;
+}
+
+/**
+ * @param $id           integer id of recipe
+ * @return string       string that contains all relevant information as html tags
+ * information retrieved   |  <ol><li> instruction_text </li></ol>
  *
  * Gathers all of the relevant information stored in the instructions table and
  * returns it as a string formatted as html tags
  */
 function getInstructions($id){
-    ob_start();
     $db = new database();
-    ob_end_clean();
     $query = "SELECT * FROM instructions WHERE rec_id=" . $id . " ORDER BY order_num";
     $relevant = array("instruction_text");
     $result = $db->sendCommandParse($query,$relevant);
-    ob_start();
     $db = null;
-    ob_end_clean();
-    $output = "";
+    $output = "<ol>";
     for ($i = 1; $i < count($result); $i++){
         $output .= "<li>" . $result[$i] . "</li>";
     }
+    $output .= "</ol>";
     return $output;
 }
 
 /**
  * @param $id           integer id of recipe
  * @return string       string that contains all relevant information as html tags
- *information retrieved  |  <li> measurement name </li>
+ *information retrieved  |  <ul><li> measurement name </li></ul>
  *
  * Gathers all relevant information stored in the ingredients table and
  * return them as a string formatted as html tags
  */
 function getIngredients($id){
-    ob_start();
     $db = new database();
-    ob_end_clean();
     $query = "SELECT * FROM ingredients WHERE rec_id=" . $id . " ORDER BY order_num";
     $relevant = array("measurement", "name");
     $result = $db->sendCommandParse($query, $relevant);
-    ob_start();
     $db = null;
-    ob_end_clean();
-    $output = "";
+    $output = "<ul>";
     for ($i = 1; $i < count($result); $i += 2){
         $output .= "<li>" . $result[$i] . " " . $result[$i+1] . "</li>";
     }
+    $output .= "</ul>";
     return $output;
 }
 
 //Example usage
-//echo getTitle(1) . "\n";
-//echo getIngredients(1) . "\n";
-//echo getInstructions(1);
+//$result = getRecipe(1);
+//echo $result[1] . "\n";
+//echo $result[2] . "\n";
+//echo $result[3];

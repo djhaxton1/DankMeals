c76ecbdc7b9f5b1976e88bc8ee152c34e81bce98
mslay <Michaellay1313@gmail.com>
2016-10-10 14:30:31 -0400

    Added dbController
    Added file that accepts accepts requests for database information and
    returns them as associative array json objects

---
+++ dbController.php
@@ -0,0 +1,22 @@
+<?php
+require 'database/dbInterface.php';
+
+$id = $_POST["argument"];   //recipe id that is passed to the page
+if (getttype($id) != "integer"){
+    header("HTTP/1.1 400 Bad Request");
+    echo "Invalid ID";
+}
+$dbInterface = new dbInterface();   //the dbInterface object used to access database
+
+//determine which function has been called
+switch ($_POST["function"]){
+    case ("getRecipeList()"):
+        echo json_encode($dbInterface->getRecipeList());
+        break;
+    case ("getRecipe()"):
+        echo json_encode($dbInterface->getRecipe($id));
+        break;
+    default:
+        header("HTTP/1.1 400 Bad Request");
+        echo "No such function";
+}

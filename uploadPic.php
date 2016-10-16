0bc63a33141365bc6617e6b9d03f9efc878b173f
Michael Miller <mikemandm222@gmail.com>
2016-10-16 13:44:25 -0400

    added basic file upload

--- uploadPic.php
+++ uploadPic.php
@@ -1,3 +1,32 @@
 <?php
-	echo "new place where you upload pictures";
+if(isset($_FILES['image'])){
+		$errors= array();
+		$file_name = $_FILES['image']['name'];
+		$file_size =$_FILES['image']['size'];
+		$file_tmp =$_FILES['image']['tmp_name'];
+		$file_type=$_FILES['image']['type'];
+
+		if($file_size > 2097152){
+			 $errors[]='File size must be excately 2 MB';
+		}
+
+		if(empty($errors)==true){
+			$dirpath = realpath(dirname(getcwd()));
+			echo $dirpath;
+			 move_uploaded_file($file_tmp, $dirpath . "/DankMeals/pics/" . $file_name);
+			 echo "Success";
+		}else{
+			 print_r($errors);
+		}
+ }
 ?>
+<html>
+   <body>
+
+      <form action="" method="POST" enctype="multipart/form-data">
+         <input type="file" name="image" />
+         <input type="submit"/>
+      </form>
+
+   </body>
+</html>

2dd3bdce8b5102b7cec9875e619db4af5911d1f1
Michael Miller <milmille@c28-0117-12.ad.mtu.edu>
2016-10-17 14:21:34 -0400

    added ajax call in html that calls php. not tested yet.

--- uploadPic.php
+++ uploadPic.php
@@ -20,13 +20,3 @@ if(isset($_FILES['image'])){
 		}
  }
 ?>
-<html>
-   <body>
-
-      <form action="" method="POST" enctype="multipart/form-data">
-         <input type="file" name="image" />
-         <input type="submit"/>
-      </form>
-
-   </body>
-</html>

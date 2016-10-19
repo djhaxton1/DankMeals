5b23f79599c931a8ab9b0a5b9e9b4a5f5099e42e
Michael Miller <mikemandm222@gmail.com>
2016-10-18 21:19:43 -0400

    removed ajax call. now renames file and places it in correct dir.

--- uploadPic.php
+++ uploadPic.php
@@ -1,4 +1,5 @@
 <?php
+require 'database\database.php';
 if(isset($_FILES['image'])){
 		$errors= array();
 		$file_name = $_FILES['image']['name'];
@@ -11,10 +12,22 @@ if(isset($_FILES['image'])){
 		}
 
 		if(empty($errors)==true){
+
+			ob_start();
+			$db = new database();
+			ob_end_clean();
+			$query = "SELECT last_insert_id();";
+			$relevant = array("last_insert_id()");
+			$db->sendCommand("INSERT INTO recipes values(null, null, 'dummy data', 'dummy pic');");
+			$result = $db->sendCommandParse($query, $relevant);
+			$id = $result[1];
+			ob_start();
+			$db = null;
+			ob_end_clean();
+
 			$dirpath = realpath(dirname(getcwd()));
-			echo $dirpath;
-			 move_uploaded_file($file_tmp, $dirpath . "/DankMeals/pics/" . $file_name);
-			 echo "Success";
+			mkdir($dirpath . "/DankMeals/pics/rec" . $id);
+			move_uploaded_file($file_tmp, $dirpath . "/DankMeals/pics/rec" . $id . "/rec" . $id . "_0.jpg");
 		}else{
 			 print_r($errors);
 		}

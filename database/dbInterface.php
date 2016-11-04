b88121f3873973436b6bd1174fe100f062e8d3c4
Dustin J Haxton <djhaxton@mtu.edu>
2016-11-04 13:58:59 -0400

    Adding functionality to add recipe page

--- database/dbInterface.php
+++ database/dbInterface.php
@@ -19,11 +19,11 @@ class dbInterface{
 
     /**
      * @return array    an associative array containing basic data for all recipes where each index is a single entry
-     *                  Data Currently Output:
-     *                      array of recipe ids in $output["ids"]
-     *                      array of recipe titles in $output["titles"]
-     *                      integer that is the number of recipe entries in $output["length"]
-     *                      array of recipe pictures in $output["pictures"]
+	 * Data Currently Output:
+     * array of recipe ids in $output["ids"]
+     * array of recipe titles in $output["titles"]
+     * integer that is the number of recipe entries in $output["length"]
+     * array of recipe pictures in $output["pictures"]
      */
     function getRecipeList(){
         $output = array();
@@ -46,11 +46,11 @@ class dbInterface{
     /**
      * @param $id   the id of the recipe requested
      * @return array    associative array containing all relevant data for a single recipe entry
-     *                  Data Currently Output:
-     *                      title string in $output["title"]
-     *                      array of ingredient strings in $output["ingredients"]
-     *                      array of instruction strings in $output["instructions"]
-     *                      string that contains the picture path in $output["picture"]
+     * Data Currently Output:
+     * title string in $output["title"]
+     * array of ingredient strings in $output["ingredients"]
+     * array of instruction strings in $output["instructions"]
+     * string that contains the picture path in $output["picture"]
      */
     function getRecipe($id){
         $output = array();
@@ -70,38 +70,42 @@ class dbInterface{
 
     /**
      * @param $data     an associative array that contains information in the following format
-     *                      data["title"]: the name of the recipe as a string
-     *                      data["parent_id"]: the id of the parent recipe as an integer  |  NUll if no parent
-     *                      data["ingredient_measurement"]: an array of ingredient measurement strings in the order they appear
-     *                      data["ingredient_name"]: an array of ingredient name strings in the order they appear
-     *                      data["instructions"]: an array of instruction text strings in the order they appear
+     * data["title"]:                  the name of the recipe as a string
+     * data["parent_id"]:              the id of the parent recipe as an integer | NUll if no parent
+     * data["ingredient_measurement"]: an array of ingredient measurement strings in the order they appear
+     * data["ingredient_name"]:        an array of ingredient name strings in the order they appear
+     * data["instructions"]:           an array of instruction text strings in the order they appear
      *
      * @return int     ID of the newly added recipe in the recipes table
      *
      * Inserts a new recipe and its associated instructions and ingredients
-     *     Note:  this function assumes that a correctly formatted directory will be built for the picture file
+     * Note:  this function assumes that a correctly formatted directory will be built for the picture file
      */
     function InsertRecipe($data){
         //TODO validate argument
         $id = -1;
+       
         //insert main recipe entry
-        $query = "INSERT INTO recipes (parent_id, title, picture) VALUES (" . data["parent_id"] . ", " . data["title"] . ",'')";
+        $query = "INSERT INTO recipes (parent_id, title, picture) VALUES (" . $data["parent_id"] . ", " . $data["title"] . ",'')";
         $this->db->sendCommand($query);
         $id = $this->db->getLastID();   //find the id of the newly inserted recipe
+        echo "$id";
         //insert the assumed path to the picture file
         $directory = "/rec" . $id ."/rec" . $id . "_0.jpg";
         $query = "UPDATE recipes SET picture=" . $directory . " WHERE id=" . $id;
         $this->db->sendCommand($query);
+        
         //insert ingredients to ingredients table
         for ($i = 0; i < count(data["ingredient_measurement"]); $i++){
             $query = "INSERT INTO ingredients (rec_id, order_num, measurement, name) VALUES (" . $id . ", " .
-                $i+1 . ", " . data["ingredient_measurement"][$id] . ", " . data["ingredient_name"][$id] . ")";
+            $i+1 . ", " . $data["ingredient_measurement"][$i] . ", " . $data["ingredient_name"][$i] . ")";
             $this->db->sendCommand($query);
         }
+        
         //insert instructions to instructions table
         for ($i = 0; i < count(data["instructions"]); $i++){
             $query = "INSERT INTO instructions (rec_id, order_num, instruction_text) VALUES (" . $id . ", " .
-                $i+1 . ", " . data["instructions"][$id];
+            $i+1 . ", " . $data["instructions"][$i];
             $this->db->sendCommand($query);
         }
         return $id; //the id of the new entry in the recipes table
